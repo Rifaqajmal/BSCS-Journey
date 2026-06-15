bc4105cf4bda58f6496ab759ee7a3dbd072f8ee0
Rifaq Ajmal Mohmand <rifaqajmalmohmand@gmail.com>
2026-06-15 20:45:21 +0500

    Delete Semester-3 directory

--- Semester-3/DBMS/10_subqueries.sql
+++
@@ -1,6 +0,0 @@
--- Task: Subqueries
-
--- Students older than average age
-SELECT *
-FROM Student
-WHERE age > (SELECT AVG(age) FROM Student);
--- Semester-3/DBMS/11_views.sql
+++
@@ -1,8 +0,0 @@
--- Task: Views
-
--- Create View
-CREATE VIEW CS_Students AS
-SELECT * FROM Student WHERE department = 'CS';
-
--- Use View
-SELECT * FROM CS_Students;
